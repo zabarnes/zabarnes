7b997bd56d88b103fae78610359d26f507047416
Zach Barnes <zach.a.barnes@gmail.com>
2021-08-09 18:18:36 -0700

    config

--- scripts/publish.py
+++ scripts/publish.py
@@ -6,10 +6,8 @@ from datetime import datetime
 
 if __name__ == '__main__':
   for nb_path in sys.argv[1:]:
-    print("setting meta for", nb_path)
     nb = nbformat.read(nb_path, nbformat.NO_CONVERT)
-    new_title =" ".join(map(str.capitalize, nb_path.replace(".ipynb","").split("/")[-1].split("-")[3:]))
-    print("new title", new_title)
+    new_title = " ".join(map(str.capitalize, nb_path.replace(".ipynb","").split("/")[-1].split("-")[3:]))
     nb.metadata['jekyll'] = {
       "layout": "single",
       "title": new_title,
